3d09e363c2f2d32f6b8f9025c2eccd28546ba037
Nikita D <dnawastaken@gmail.com>
2026-03-18 12:19:28 -0700

    create a zip of the code for LLMs to have full code access

---
+++ zip-code.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env zsh
+
+OUTPUT="iphone-to-pixel-code.zip"
+
+zip -r "$OUTPUT" \
+  src \
+  src-ui \
+  src-tauri/src \
+  src-tauri/Cargo.toml \
+  src-tauri/tauri.conf.json \
+  package.json \
+  TASKS.md \
+  google-photos-metadata-fix-and-reupload-strategy.md \
+  -x "*/node_modules/*" \
+  -x "*/dist/*" \
+  -x "*/.git/*"
+
+echo "Created $OUTPUT"
