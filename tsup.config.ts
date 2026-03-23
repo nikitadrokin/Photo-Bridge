656cfdb02545fdf37069031ef5e3bfce0754666b
Nikita D <dnawastaken@gmail.com>
2026-03-22 22:32:15 -0700

    separated cli and tauri into workspaces

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'tsup';
 export default defineConfig({
   clean: true,
   dts: true,
-  entry: ['src/index.ts'],
+  entry: ['src-cli/index.ts'],
   format: ['esm'],
   sourcemap: true,
   minify: true,
