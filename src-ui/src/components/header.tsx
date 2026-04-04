8d10206786e0564e632333368e923aa3082c307e
Nikita D <dnawastaken@gmail.com>
2026-04-03 20:22:25 -0700

    select none

--- src-ui/src/components/header.tsx
+++ src-ui/src/components/header.tsx
@@ -18,7 +18,7 @@ export function PageHeader({ title, children }: PageHeaderProps) {
     <header
       data-tauri-drag-region
       className={cn(
-        'z-10 shrink-0 border-b mx-2 border-border bg-background/80 backdrop-blur-lg px-4 py-3 transition-[padding] duration-200 ease-in-out',
+        'z-10 shrink-0 border-b mx-2 border-border bg-background/80 backdrop-blur-lg px-4 py-3 transition-[padding] duration-200 ease-in-out select-none [-webkit-user-select:none] [-webkit-touch-callout:none]',
         !isFullscreen && (!sidebarOpen || isMobile) && 'pl-26',
       )}
     >
