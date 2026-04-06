5b57b0dc55ef50701d640ba822bbb797bf34d842
Nikita D <dnawastaken@gmail.com>
2026-04-06 15:39:55 -0700

    removed audio extensions from legacy video list

--- src-cli/utils/constants.ts
+++ src-cli/utils/constants.ts
@@ -2,6 +2,6 @@
 export const IMAGE_EXTENSIONS = ['heic', 'heif', 'jpg', 'jpeg', 'png', 'gif', 'dng', 'webp'];
 export const VIDEO_EXTENSIONS = ['mov', 'mp4', 'm4v'];
 // prettier-ignore
-export const LEGACY_VIDEO_EXTENSIONS = ['avi', 'wmv', 'mpeg', 'mpg', 'm4v', 'm4a'];
+export const LEGACY_VIDEO_EXTENSIONS = ['avi', 'wmv', 'mpeg', 'mpg'];
 // prettier-ignore
 export const ALL_EXTENSIONS = [...IMAGE_EXTENSIONS, ...VIDEO_EXTENSIONS, ...LEGACY_VIDEO_EXTENSIONS];
