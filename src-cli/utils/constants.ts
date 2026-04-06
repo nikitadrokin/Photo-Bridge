5fa8c1ce9ed1f5e74a33673aed217dfaeb6cde10
Nikita D <dnawastaken@gmail.com>
2026-04-06 15:14:11 -0700

    added filetype constants

---
+++ src-cli/utils/constants.ts
@@ -0,0 +1,6 @@
+// prettier-ignore
+export const IMAGE_EXTENSIONS = ['heic', 'heif', 'jpg', 'jpeg', 'png', 'gif', 'dng', 'webp'];
+export const VIDEO_EXTENSIONS = ['mov', 'mp4', 'm4v'];
+// prettier-ignore
+export const LEGACY_VIDEO_EXTENSIONS = ['avi', 'wmv', 'mpeg', 'mpg', 'm4v', 'm4a'];
+export const ALL_EXTENSIONS = [...IMAGE_EXTENSIONS, ...VIDEO_EXTENSIONS];
