127ce18f38e9ab15056804f04871f145cf40aa6f
Nikita D <dnawastaken@gmail.com>
2026-06-05 00:04:49 -0700

    header

--- src-ui/src/components/header.tsx
+++ src-ui/src/components/header.tsx
@@ -15,7 +15,7 @@ export function PageHeader({ title, children, ...props }: PageHeaderProps) {
     <header
       data-tauri-drag-region
       className={cn(
-        'shrink-0 z-20 bg-sidebar w-full pr-2 py-3 backdrop-blur-lg transition-[padding] duration-200 ease-in-out select-none [-webkit-user-select:none] [-webkit-touch-callout:none]',
+        'shrink-0 z-20 bg-sidebar w-full pr-2 py-2 backdrop-blur-lg transition-[padding] duration-200 ease-in-out select-none [-webkit-user-select:none] [-webkit-touch-callout:none]',
         !isFullscreen ? 'pl-26' : 'pl-4',
       )}
       {...props}
