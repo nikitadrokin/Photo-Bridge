91c9c7f718bd6368bada3738481c5e8ad65084a6
Nikita D <dnawastaken@gmail.com>
2026-06-16 19:21:35 -0700

    border radius on fullscreen

--- src-ui/src/components/empty-dropzone.tsx
+++ src-ui/src/components/empty-dropzone.tsx
@@ -1,4 +1,5 @@
 import { cn } from '@/lib/utils';
+import useIsFullscreen from '@/hooks/use-is-fullscreen';
 
 interface EmptyDropzoneProps {
   isDragging?: boolean;
@@ -9,10 +10,13 @@ const EmptyDropzone: React.FC<EmptyDropzoneProps> = ({
   isDragging,
   children,
 }) => {
+  const isFullscreen = useIsFullscreen();
+
   return (
     <div
       className={cn(
         'flex flex-col items-center justify-center rounded-xl border-2 border-dashed m-1 grow py-16 px-8 text-center transition-colors duration-200 col-span-full',
+        !isFullscreen && 'md:rounded-br-md',
         isDragging
           ? 'border-primary bg-primary/10'
           : 'border-border/60 bg-muted/20 hover:border-border hover:bg-muted/30',
