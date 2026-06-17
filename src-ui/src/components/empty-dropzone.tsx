944178ca5164d95982c0f71edb7bc3dbcbfa6a95
Nikita D <dnawastaken@gmail.com>
2026-06-16 19:22:18 -0700

    sidebar open

--- src-ui/src/components/empty-dropzone.tsx
+++ src-ui/src/components/empty-dropzone.tsx
@@ -1,5 +1,6 @@
 import { cn } from '@/lib/utils';
 import useIsFullscreen from '@/hooks/use-is-fullscreen';
+import { useSidebar } from './ui/sidebar';
 
 interface EmptyDropzoneProps {
   isDragging?: boolean;
@@ -11,12 +12,13 @@ const EmptyDropzone: React.FC<EmptyDropzoneProps> = ({
   children,
 }) => {
   const isFullscreen = useIsFullscreen();
+  const { open } = useSidebar();
 
   return (
     <div
       className={cn(
         'flex flex-col items-center justify-center rounded-xl border-2 border-dashed m-1 grow py-16 px-8 text-center transition-colors duration-200 col-span-full',
-        !isFullscreen && 'md:rounded-br-md',
+        !isFullscreen && open && 'md:rounded-br-md',
         isDragging
           ? 'border-primary bg-primary/10'
           : 'border-border/60 bg-muted/20 hover:border-border hover:bg-muted/30',
